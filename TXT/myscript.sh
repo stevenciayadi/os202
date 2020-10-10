4a2ede78b906456c6f67b5aa79caf33bc750322b
Steven Ciayadi <stevenciayadi23@gmail.com>
2020-10-10 18:56:14 +0700

    OS202 stevenciayadi

--- TXT/myscript.sh
+++ TXT/myscript.sh
@@ -1,10 +1,11 @@
+
 #!/bin/bash
 # Cicak bin Kadal
 # Mon Sep 28 22:03:27 WIB 2020
 # Tue 29 Sep 2020 11:02:39 AM WIB
 # PALSU BANGET SEKALI UBAH
 
-FILES="mylog.txt mypubkey.txt myrank.txt myscript.sh"
+FILES="my*.txt my*.sh"
 SHA="SHA256SUM"
 
 rm -f $SHA $SHA.asc
@@ -22,4 +23,29 @@ echo "gpg --verify $SHA.asc $SHA"
 gpg --verify $SHA.asc $SHA
 
 exit 0
+
+# Mon Sep 28 21:05:04 WIB 2020
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
